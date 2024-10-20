7adf879c2843691b04352ca9be33945802815ea1
Peter Yuan <peter.yuan18@gmail.com>
2024-10-19 21:38:49 -0400

    Add generalized swiss bracket round generation

--- SwissBracket.ts
+++ SwissBracket.ts
@@ -1,54 +1,50 @@
 import { RoundNode } from "./models.ts";
 
 export function SwissBracket(numTeams: number = 16, winRequirement: number = 3) {
-	const root = new RoundNode("r1", numTeams, 0, 0);
+	const root = new RoundNode("0-0", numTeams, 0, 0);
 	let queue: RoundNode[] = [];
 	queue.push(root);
 	while (queue.length > 0) {
 		const existingNodes: Map<string, RoundNode> = new Map();
-		let maxWins = 0;
 		const newQueue: RoundNode[] = [];
 		for (let i = 0; i < queue.length; i++) {
 			const node = queue[i];
-			if (node) {
+			// update winning child
+			if (node.winRecord + 1 < winRequirement) {
 				const winningNodeRecord = `${node.winRecord + 1}-${node.loseRecord}`;
-				const losingNodeRecord = `${node.winRecord}-${node.loseRecord + 1}`;
-
 				checkAndAddNode(existingNodes, winningNodeRecord, node, 1, 0);
-				checkAndAddNode(existingNodes, losingNodeRecord, node, 0, 1);
-
 				node.winningRound = existingNodes.get(winningNodeRecord);
+			}
+			// update losing child
+			if (node.loseRecord + 1 < winRequirement) {
+				const losingNodeRecord = `${node.winRecord}-${node.loseRecord + 1}`;
+				checkAndAddNode(existingNodes, losingNodeRecord, node, 0, 1);
 				node.losingRound = existingNodes.get(losingNodeRecord);
-
-				if (node.winningRound) {
-					newQueue.push(node.winningRound);
-				}
-				if (node.losingRound) {
-					newQueue.push(node.losingRound);
-				}
-				maxWins = Math.max(maxWins, node.winRecord + 1);
 			}
 		}
+		existingNodes.forEach((value) => {
+			newQueue.push(value);
+		});
 		queue = newQueue;
-		if (maxWins === winRequirement - 1) {
-			break;
-		}
 	}
 
 	levelOrderTraversal(root);
-	console.log(root);
 }
 
 // prints out swiss rounds level by level
-// currently prints "middle" rounds (rounds nodes that have 2 parents) twice
+// will print each RoundNode once
 function levelOrderTraversal(root: RoundNode) {
 	const queue: RoundNode[] = [];
+	const visited: string[] = [];
 	queue.push(root);
 	while (queue.length > 0) {
 		for (let i = 0; i < queue.length; i++) {
 			const node = queue.shift();
 			if (node) {
-				console.log(node.toString());
+				if (visited.indexOf(node.name) == -1) {
+					console.log(node.toString());
+					visited.push(node.name);
+				}
 				if (node.winningRound) {
 					queue.push(node.winningRound);
 				}
@@ -73,7 +69,7 @@ function checkAndAddNode(
 		return false;
 	} else {
 		const newNode = new RoundNode(
-			"temp",
+			nodeRecord,
 			parentNode.numTeams / 2,
 			parentNode.winRecord + addWinRecord,
 			parentNode.loseRecord + addLoseRecord
