f9cd78328e5bb2180ed1d7a87b233369a7a6aad2
Peter Yuan <peter.yuan18@gmail.com>
2024-12-26 12:08:35 -0500

    Add afl constructor

--- src/afl_bracket/afl_bracket.ts
+++ src/afl_bracket/afl_bracket.ts
@@ -4,10 +4,26 @@ import { RoundNode } from "../models/round_node.ts";
 export class AFLBracket extends Bracket {
 	override rootRound: RoundNode;
 
+	// by definition, there are 8 seeds for this bracket
 	constructor() {
-        super();
-		this.rootRound = new RoundNode("dummy", 0, 0, 0, 0);
+		super();
+		this.rootRound = new RoundNode("0-0", 0, 0, 0, 0);
 
-		
+		const upperFinals = new RoundNode("1-0", 4, 0, 0, 1);
+		this.rootRound.winningRound = upperFinals;
+
+		const lowerBracketRound1 = new RoundNode("0-1", 4, 0, 0, 1);
+		this.rootRound.losingRound = lowerBracketRound1;
+
+		const lowerBracketQuarterFinals = new RoundNode("1-1", 4, 0, 0, 2);
+		upperFinals.losingRound = lowerBracketQuarterFinals;
+		lowerBracketRound1.winningRound = lowerBracketQuarterFinals;
+
+		const semiFinals = new RoundNode("2-1", 4, 0, 0, 3);
+		upperFinals.winningRound = semiFinals;
+		lowerBracketQuarterFinals.winningRound = semiFinals;
+
+		const grandFinal = new RoundNode("3-1", 2, 0, 0, 4);
+		semiFinals.winningRound = grandFinal;
 	}
 }
