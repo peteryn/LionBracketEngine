59fa16788745bc2cbd48f1747c593cbf7a455c1e
Peter Yuan <peter.yuan18@gmail.com>
2025-01-02 19:36:05 -0500

    Remove commented code

--- src/models/match_record.ts
+++ src/models/match_record.ts
@@ -1,19 +1,5 @@
 export type Seed = number;
 
-// export class MatchRecord {
-// 	upperSeed: Seed;
-// 	lowerSeed: Seed;
-// 	upperSeedWins: number;
-// 	lowerSeedWins: number;
-
-// 	constructor(upperSeed: Seed, lowerSeed: Seed) {
-// 		this.upperSeed = upperSeed;
-// 		this.lowerSeed = lowerSeed;
-// 		this.upperSeedWins = 0;
-// 		this.lowerSeedWins = 0;
-// 	}
-// }
-
 export type MatchId = {
 	matchId: string;
 };
