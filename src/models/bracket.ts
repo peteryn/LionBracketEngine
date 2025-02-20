31b7800b6445de45bf9b4362c0aad3c4fbd156a7
Peter Yuan <peter.yuan18@gmail.com>
2025-02-20 11:19:11 -0500

    Add experimental types

--- src/models/bracket.ts
+++ src/models/bracket.ts
@@ -1,3 +1,5 @@
+import { AFLBracketFlow } from "../afl_bracket/afl_bracket_flow.ts";
+import { SwissBracketFlow } from "../swiss_bracket/swiss_backet_flow.ts";
 import { BracketNode } from "./bracket_node.ts";
 import { Match } from "./match.ts";
 import { MatchRecord } from "./match_record.ts";
@@ -12,4 +14,14 @@ export interface Bracket<NodeType extends BracketNode> {
 	setMatchRecord(matchId: string, matchRecord: MatchRecord): boolean;
 
 	setMatchRecordWithValue(matchId: string, upperSeedWins: number, lowerSeedWins: number): boolean;
+}
+
+export type Major1SwissBracket = {
+	bracketType: "M1SwissBracket"
+	bracketObject: SwissBracketFlow
+}
+
+export type Major1AFLBracket = {
+	bracketType: "M1AFLBracket"
+	bracketObject: AFLBracketFlow
 }
