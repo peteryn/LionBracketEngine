fd9414b8374e54e2e997a10554bb20f1b219b6aa
Peter Yuan <peter.yuan18@gmail.com>
2025-02-28 18:09:19 -0500

    Start new swiss bracket

--- src/swiss_bracket/swiss_bracket_flow_8apart.ts
+++ src/swiss_bracket/swiss_bracket_flow_8apart.ts
@@ -1,6 +1,8 @@
 import { Seed } from "../models/match_record.ts";
+import { RoundNode } from "../models/round_node.ts";
 import { createSeeds, eightApartMatchups, populateMatches } from "../util/util.ts";
 import { SwissBracketFlow } from "./swiss_backet_flow.ts";
+import { levelOrderTraversal } from "../util/util.ts";
 
 export class SwissBracketFlow8Apart extends SwissBracketFlow {
 	constructor(numSeeds: number = 16, winRequirement: number = 3) {
@@ -13,5 +15,18 @@ export class SwissBracketFlow8Apart extends SwissBracketFlow {
 
 	override tieBreaker(seed: Seed): number {
 		return this.getBuchholzScore(seed);
+	}
+
+	override updateFlow(roundNode: RoundNode): void {
+		const curLevel = roundNode.level;
+		const level: RoundNode[] = [];
+		levelOrderTraversal(this.rootRound, (node) => {
+			if (node.level === curLevel) {
+				level.push(node);
+			}
+		})
+		
+		// entire level needs to be filled out before any future rounds can be calculated
+
 	}
 }
