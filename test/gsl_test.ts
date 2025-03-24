29461354b73ec4b1ba3ca586a53579b6c7137deb
Peter Yuan <peter.yuan18@gmail.com>
2025-03-24 12:33:53 -0400

    Simplify gsl test code

--- test/gsl_test.ts
+++ test/gsl_test.ts
@@ -1,6 +1,4 @@
-import { assertEquals } from "@std/assert/equals";
 import { GSLBracketFlow } from "../src/gsl_bracket/gsl_bracket_flow.ts";
-import { FullRecord } from "../src/models/match_record.ts";
 import { checkMatchNodeSeeds } from "./util/testFunctions.ts";
 
 Deno.test(function gslTest1() {
@@ -10,37 +8,16 @@ Deno.test(function gslTest1() {
 	gslBracket.setMatchRecordAndFlow("UpperQuarterFinal3", 1, 0);
 	gslBracket.setMatchRecordAndFlow("UpperQuarterFinal4", 1, 0);
 
-	const upperSemiFinal1 = gslBracket.getBracketNode("UpperSemiFinal1");
-	assertEquals(upperSemiFinal1.match.matchRecord?.type, "FullRecord");
-	const uSF1MR = upperSemiFinal1.match.matchRecord as FullRecord;
-	assertEquals(uSF1MR.upperSeed, 1);
-	assertEquals(uSF1MR.lowerSeed, 4);
+	checkMatchNodeSeeds(gslBracket, "UpperSemiFinal1", 1, 4);
+	checkMatchNodeSeeds(gslBracket, "UpperSemiFinal2", 2, 3);
 
-	const upperSemiFinal2 = gslBracket.getBracketNode("UpperSemiFinal2");
-	assertEquals(upperSemiFinal2.match.matchRecord?.type, "FullRecord");
-	const uSF2MR = upperSemiFinal2.match.matchRecord as FullRecord;
-	assertEquals(uSF2MR.upperSeed, 2);
-	assertEquals(uSF2MR.lowerSeed, 3);
-
-	const lowerQuarterFinal1 = gslBracket.getBracketNode("LowerQuarterFinal1");
-	assertEquals(lowerQuarterFinal1.match.matchRecord?.type, "FullRecord");
-	const lQF1MR = lowerQuarterFinal1.match.matchRecord as FullRecord;
-	assertEquals(lQF1MR.upperSeed, 8);
-	assertEquals(lQF1MR.lowerSeed, 5);
-
-	const lowerQuarterFinal2 = gslBracket.getBracketNode("LowerQuarterFinal2");
-	assertEquals(lowerQuarterFinal2.match.matchRecord?.type, "FullRecord");
-	const lQF2MR = lowerQuarterFinal2.match.matchRecord as FullRecord;
-	assertEquals(lQF2MR.upperSeed, 7);
-	assertEquals(lQF2MR.lowerSeed, 6);
+	checkMatchNodeSeeds(gslBracket, "LowerQuarterFinal1", 8, 5);
+	checkMatchNodeSeeds(gslBracket, "LowerQuarterFinal2", 7, 6);
 
 	gslBracket.setMatchRecordAndFlow("UpperSemiFinal1", 1, 0);
 	gslBracket.setMatchRecordAndFlow("UpperSemiFinal2", 1, 0);
-	const upperFinal = gslBracket.getBracketNode("UpperFinal");
-	assertEquals(upperFinal.match.matchRecord?.type, "FullRecord");
-	const uFMR = upperFinal.match.matchRecord as FullRecord;
-	assertEquals(uFMR.upperSeed, 1);
-	assertEquals(uFMR.lowerSeed, 2);
+
+	checkMatchNodeSeeds(gslBracket, "UpperFinal", 1, 2);
 
 	gslBracket.setMatchRecordAndFlow("LowerQuarterFinal1", 1, 0);
 	gslBracket.setMatchRecordAndFlow("LowerQuarterFinal2", 1, 0);
